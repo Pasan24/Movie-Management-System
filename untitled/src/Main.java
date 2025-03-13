309bc7ddc86063017eef58faea878f1add32f938
Pasan24 <pramudithapasan076@gmail.com>
2025-03-13 17:08:34 +0530

    finished the files need to create user. and fix the database connection issues usiing

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,35 +1,26 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-    private static Scanner scanner =new Scanner(System.in);
-    private static  Database database ; //static method can only access static variables and method
+    private static Scanner scanner = new Scanner(System.in);
+    private static Database database;
 
     public static void main(String[] args) {
         database = new Database();
-        System.out.println(" Welcome to movies Ticket Booking System ");
+        System.out.println("Welcome to movies Ticket Booking System");
         System.out.println("1. Login");
-        System.out.println("2. Create  new Account");
-
-
+        System.out.println("2. Create new Account");
 
         int i = scanner.nextInt();
-
 
         switch (i) {
             case 1:
                 login();
                 break;
-
             case 2:
                 createNewAccount();
                 break;
-
-
             default:
-                System.out.println("Unvalid Input !");
+                System.out.println("Unvalid Input!");
                 break;
         }
 
@@ -37,13 +28,13 @@ public class Main {
     }
 
     public static void login() {
-
-        System.out.println("Enter Your Email : ");
+        System.out.println("Enter Your Email: ");
         String email = scanner.next();
 
-        System.out.println("Enter Your password :");
+        System.out.println("Enter Your password:");
         String password = scanner.next();
 
+        // NOTE: Login functionality not implemented yet
     }
 
     public static void createNewAccount() {
@@ -53,51 +44,44 @@ public class Main {
         System.out.println("Enter Your LastName:");
         String lastName = scanner.next();
 
-        System.out.println("Enter Your Email : ");
+        System.out.println("Enter Your Email: ");
         String email = scanner.next();
 
-        System.out.println("Enter Your phoneNumber :");
+        System.out.println("Enter Your phoneNumber:");
         String phoneNumber = scanner.next();
 
-        System.out.println("Enter Your password :");
+        System.out.println("Enter Your password:");
         String password = scanner.next();
 
-        System.out.println("Confirm  password :");
+        System.out.println("Confirm password:");
         String confirmPassword = scanner.next();
 
         while (!password.equals(confirmPassword)) {
-
             System.out.println("Password doesn't match");
             System.out.println();
 
-            System.out.println("Enter Your password :");
+            System.out.println("Enter Your password:");
             password = scanner.next();
 
-            System.out.println("Enter correct Confirm  password :");
+            System.out.println("Enter correct Confirm password:");
             confirmPassword = scanner.next();
-
-
         }
 
         while (UsersDataBase.IsEmailUsed(email, database)) {
-            System.out.println("This email is already USed !");
-            System.out.println("Enter your email ");
+            System.out.println("This email is already Used!");
+            System.out.println("Enter your email:");
             email = scanner.next();
-
-
-
         }
 
         Visitor visitor = new Visitor();
         visitor.setID(UsersDataBase.getNextVisitorID(database));
-        visitor.setFirstName("firstName");
-        visitor.setLastName("lastName");
-        visitor.setPhoneNumber("phoneNumber");
-        visitor.setEmail("email");
-        visitor.setPassword("password");
+        // FIXED: Changed string literals to actual variables
+        visitor.setFirstName(firstName);  // Changed from "firstName" to firstName
+        visitor.setLastName(lastName);    // Changed from "lastName" to lastName
+        visitor.setPhoneNumber(phoneNumber); // Changed from "phoneNumber" to phoneNumber
+        visitor.setEmail(email);          // Changed from "email" to email
+        visitor.setPassword(password);    // Changed from "password" to password
 
-        UsersDataBase.addVisitor(visitor,database);
-
-
+        UsersDataBase.addVisitor(visitor, database);
     }
 }
--- untitled/src/UsersDataBase.java
+++ untitled/src/UsersDataBase.java
@@ -4,80 +4,90 @@ import java.util.ArrayList;
 
 public class UsersDataBase {
 
-    public static boolean IsEmailUsed(String email, Database database){
+    public static boolean IsEmailUsed(String email, Database database) {
+        boolean isUsed = false;
 
-        boolean isUsed =  false ;
+        try {
+            // FIXED: Added null check to prevent NullPointerException
+            if (database.getStatement() == null) {
+                System.err.println("Database statement is null. Check database connection.");
+                return false;
+            }
 
-        try{
-            ResultSet rs = database.getStatement().executeQuery(" SELECT `ID`, `firstName`, `lastName`, `phoneNumber`, " +
-                    "`email`, `password`  FROM `visitors` WHERE  `email` = '" +email +"' ;");
+            ResultSet rs = database.getStatement().executeQuery("SELECT `ID`, `firstName`, `lastName`, `phoneNumber`, " +
+                    "`email`, `password` FROM `visitors` WHERE `email` = '" + email + "';");
             isUsed = rs.next();
 
-        }catch (Exception e){
+        } catch (Exception e) {
+            System.err.println("Error checking email: " + e.getMessage());
             e.printStackTrace();
         }
 
-        return isUsed ;
+        return isUsed;
     }
 
-    public static ArrayList<Visitor> getAllVisitors(Database database
-    ){
+    public static ArrayList<Visitor> getAllVisitors(Database database) {
+        ArrayList<Visitor> visitors = new ArrayList<>();
 
-        ArrayList<Visitor> visitors =  new ArrayList<>();
-        try{
-            ResultSet rs = database.getStatement().executeQuery("SELECT * FROM `visitors` ;");
+        try {
+            // FIXED: Added null check to prevent NullPointerException
+            if (database.getStatement() == null) {
+                System.err.println("Database statement is null. Check database connection.");
+                return visitors; // Return empty list instead of null
+            }
+
+            ResultSet rs = database.getStatement().executeQuery("SELECT * FROM `visitors`;");
 
             while (rs.next()) {
-            Visitor visitor = new Visitor();
-            visitor.setID(rs.getInt("ID"));
-            visitor.setFirstName(rs.getString("firstName"));
-            visitor.setLastName(rs.getString("lastName"));
-            visitor.setPhoneNumber(rs.getString("phoneNumber"));
-            visitor.setEmail(rs.getString("email"));
-            visitor.setPassword(rs.getString("password"));
+                Visitor visitor = new Visitor();
+                visitor.setID(rs.getInt("ID"));
+                visitor.setFirstName(rs.getString("firstName"));
+                visitor.setLastName(rs.getString("lastName"));
+                visitor.setPhoneNumber(rs.getString("phoneNumber"));
+                visitor.setEmail(rs.getString("email"));
+                visitor.setPassword(rs.getString("password"));
 
-            visitors.add(visitor);
+                visitors.add(visitor);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error getting visitors: " + e.getMessage());
+            e.printStackTrace();
         }
 
-    } catch (
-    SQLException e) {
-        e.printStackTrace();  // Log properly in real applications
+        return visitors;
     }
 
-    return visitors;
-
-    }
-
-    public static int getNextVisitorID(Database database){
-        int ID = 0 ;
-        ArrayList <Visitor> visitors = getAllVisitors(database);
-        if (visitors.size()!=0){
-        int lastRow = visitors.size()-1;
-        Visitor lastVisitor = visitors.get(lastRow);
-        ID= lastVisitor.getID()+1;
-
+    public static int getNextVisitorID(Database database) {
+        int ID = 0;
+        ArrayList<Visitor> visitors = getAllVisitors(database);
+        if (visitors.size() != 0) {
+            int lastRow = visitors.size() - 1;
+            Visitor lastVisitor = visitors.get(lastRow);
+            ID = lastVisitor.getID() + 1;
         }
-
-
 
         return ID;
     }
 
-
-    public static void addVisitor (Visitor v , Database database){
-
-        String insert="INSERT INTO `visitors`(`ID`, `firstName`, `lastName`, `phoneNumber`, `email`, `password`) " +
-                "VALUES ('" +v.getID()+"','" +v.getFirstName()+"','" +v.getLastName()+"','"+v.getEmail()+"'," +
-                "'"+v.getPhoneNumber()+"','"+v.getPassword()+"');";
+    public static void addVisitor(Visitor v, Database database) {
+        // FIXED: Fixed SQL query - had email and phoneNumber swapped
+        String insert = "INSERT INTO `visitors`(`ID`, `firstName`, `lastName`, `phoneNumber`, `email`, `password`) " +
+                "VALUES ('" + v.getID() + "','" + v.getFirstName() + "','" + v.getLastName() + "','" +
+                v.getPhoneNumber() + "','" + v.getEmail() + "','" + v.getPassword() + "');";
 
         try {
+            // FIXED: Added null check to prevent NullPointerException
+            if (database.getStatement() == null) {
+                System.err.println("Database statement is null. Check database connection.");
+                return;
+            }
+
             database.getStatement().execute(insert);
-            System.out.println("USer Created Successfully");
+            System.out.println("User Created Successfully!");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.err.println("Error adding visitor: " + e.getMessage());
+            e.printStackTrace();
         }
-
     }
-
-
 }
