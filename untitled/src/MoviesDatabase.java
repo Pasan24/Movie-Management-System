5b05d916cc0d412805b48bda174ee08ab7bf7e74
Pasan24 <pramudithapasan076@gmail.com>
2025-03-16 23:59:32 +0530

    created  moviedatabase

--- untitled/src/MoviesDatabase.java
+++ untitled/src/MoviesDatabase.java
@@ -4,6 +4,21 @@ public class MoviesDatabase {
 
     public static void addNewMovie(Database database , Scanner s){
 
+        System.out.println("Enter Movie Name : ");
+
+        System.out.println("Enter Movie  language : ");
+
+        System.out.println("Enter Movie  Genre  ");
+
+        System.out.println("Enter Movie running time  : ");
+
+        System.out.println("Enter Movie  Starring : ");
+
+        System.out.println("Enter Movie  Rating : ");
+
+
+
+
     }
 
 }
